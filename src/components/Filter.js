3cede222b4e42576de4811ffebd310f5ed6d3623
KarlSavinskiy <karlsavinskiy@gmail.com>
2022-08-11 19:35:42 -0400

    done

---
+++ src/components/Filter.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+export default function Filter({handleCategoryChange}) {
+    
+  
+    
+    return (
+        <select name="filter" onChange={handleCategoryChange}>
+            <option value="All">Filter by category</option>
+            <option value="Produce">Produce</option>
+            <option value="Dairy">Dairy</option>
+            <option value="Dessert">Dessert</option>
+        </select>
+    );
+  }
